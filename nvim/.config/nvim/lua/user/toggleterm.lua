17c0ffa46a3168f68c279f9797553b8d5a0accab
mapenzo-ph <masini.alessandro94@gmail.com>
2022-12-27 02:19:51 +0100

    base config complete

--- nvim/.config/nvim/lua/user/toggleterm.lua
+++ nvim/.config/nvim/lua/user/toggleterm.lua
@@ -26,7 +26,7 @@ toggleterm.setup({
 	},
 })
 
-function _G.set_terminal_keymaps()
+function Set_terminal_keymaps()
   local opts = {noremap = true}
   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
@@ -36,7 +36,7 @@ function _G.set_terminal_keymaps()
   vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
 end
 
-vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd('autocmd! TermOpen term://* lua Set_terminal_keymaps()')
 
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
