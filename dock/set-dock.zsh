b70b836d56f01606f145884e9daaf53ef8f3b7f2
mapenzo-ph <masini.alessandro94@gmail.com>
2023-01-04 17:59:23 +0100

    removed macos specific files

--- dock/set-dock.zsh
+++
@@ -1,6 +0,0 @@
-# set dock hide delay to zero
-defaults write com.apple.dock autohide-delay -int 0
-# set dock hide animation duration
-defaults write com.apple.dock autohide-time-modifier -float 0.4 
-# restart and apply
-killall Dock
